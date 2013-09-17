ead28160396f45f7cea3d84daec138b1a3eacfef
Paul Webster <pwebster@ca.ibm.com>
2013-09-17 08:57:41 -0400

    Refactored functions.

--- clojure/source/z.ex.learning.clj/src/z/ex/json.clj
+++ clojure/source/z.ex.learning.clj/src/z/ex/json.clj
@@ -3,37 +3,36 @@
 (require '[clj-http.client :as http])
 (require '(clj-json [core :as json]))
 
-(def tst [{"user" "anthonyh", "name" "Anthony", "email" "anthonyh@ca.ibm.com"},
-          {"user" "bbokowski", "name" "Boris", "email" "Boris_Bokowski@ca.ibm.com"}])
-(defn run-tst
-  ""
-  []
-  (println (get-user-map tst)))
+;(defn old-parse-component
+;  ""
+;  [str]
+;  (let [component-json (json/parse-string (clojure.string/replace str #"\\\u000a" ""))]
+;    (map (fn [entry] (get-component (get-user-map (get component-json "users")) entry)) (get component-json "assignments") )))
 
 (defn get-component
   ""
   [user-map entry]
-  (str "Component: " (get entry "component") " Owner: " (get user-map (get entry "assignee"))))
+  (str "Component: " (get entry "component") 
+       " Owner: " (get user-map (get entry "assignee"))))
 
 (defn get-user-map
   ""
   [user-array]
-  (reduce (fn [accum x] (assoc accum
-                               (get x "user")
-                               (get x "name")))
+  (reduce (fn [accum x] (assoc accum (get x "user") (get x "name")))
           {}
           user-array))
 
-
-(defn parse-component
+(defn build-list
   ""
-  [str]
-  (let [component-json (json/parse-string (clojure.string/replace str #"\\\u000a" ""))]
-    (map (fn [entry] (get-component (get-user-map (get component-json "users")) entry)) (get component-json "assignments") )))
+  [json-body]
+  (let [component-json (json/parse-string 
+                         (clojure.string/replace json-body #"\\\u000a" ""))
+        user-map (get-user-map (get component-json "users"))
+        assign-array (get component-json "assignments")]
+    (map (fn [entry] (get-component user-map entry)) assign-array)))
 
-
-(defn -main
-  "I don't do a whole lot ... yet."
+(defn get-component-json
+  "Load the componet json and print out the component vs people"
   [& args]
   (let [x (http/get "http://www.eclipse.org/eclipse/platform-ui/componentAreas.json")]
-        (parse-component (:body x))))
+        (build-list (:body x))))
